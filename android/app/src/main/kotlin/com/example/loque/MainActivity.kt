3d81cae89508e1de934a3600d44cb3ecac822d13
innomatic <innomatic@innomatic.ca>
2024-03-06 10:01:11 -0500

    fix plugin DSL issue

---
+++ android/app/src/main/kotlin/com/example/loque/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.loque
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/innomatic/loqueapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.innomatic.loqueapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
